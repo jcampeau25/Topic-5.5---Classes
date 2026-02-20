4a08c26bcb2740c2b06479bf70ee64f5834d49c6
Joshua Campeau <339729675@GoTVDSB.ca>
2026-02-20 12:55:15 -0500

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5___Classes
+{
+    public class Die
+    {
+        private int _roll;
+        private Random _generator;
+        private ConsoleColor _color;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+            _color = ConsoleColor.Gray;
+
+        }
+
+        public int Roll
+        {
+            get { return _roll; }
+        }
+
+        public ConsoleColor Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public void DrawRoll()
+        {
+            Console.ForegroundColor = _color;
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+
+
+
+
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_5._5___Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1;
+            die1 = new Die(ConsoleColor.Red);
+            die1.Color = ConsoleColor.Green;
+
+
+            die1.DrawRoll();
+            die1.RollDie();
+            die1.DrawRoll();
+        }
+    }
+}
